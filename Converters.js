53fb4cf3d23360f28f4d7ce5bf20150649d016e4
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2014-10-15 11:47:17 -0700

    Created 'better' date string lables...

--- Converters.js
+++ Converters.js
@@ -3,9 +3,9 @@ Dave_js.Converters = {
     return val;
   },
   jsTimeToString: function jsTimeToString(ms) {
-    return (new Date(ms)).toDateString();
+    return (new Date(ms)).toUTCString();
   },
   unixTimeToString: function unixTimeToString(sec) {
-    return (new Date(sec * 1000)).toDateString();
+    return (new Date(sec * 1000)).toUTCString();
   }
 };
