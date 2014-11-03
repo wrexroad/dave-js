387a5f06b5f5835ddfab03c5649385757eba15b5
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2014-11-03 15:04:39 -0800

    Fixed a lot of typos in DateFormat.js. The basic function factory now procuces expected results. Some of the formatters still need work

--- DateFormat.js
+++ DateFormat.js
@@ -1,80 +1,88 @@
-Dave_js.DateFormat = function dateFormatter(fmt){
-  return {
-    format: [].concat(fmt),
-    getDate: function getDate(date) {
-      var
-        code,
-        formatLength,
-        result = "";
-      
-      //make sure date is a Date object
-      date = new Date(date);
+Dave_js.DateFormat = function DateFormat(fmt, dateBuilder) {
 
-      //cycle through the format array 
-      format = [].concat(format);
-      while(format.length){
-        code = format.pop();
-        result += (typeof formatter[code] != 'function') ?
-          this[code](date) : code;
+  //use the Date constructor if no dateBuilder was set
+  var getDateObject = (typeof dateBuilder == "function") ?
+      dateBuilder : Date;
+
+  //make sure format is an array
+  var format = [].concat(fmt);
+  
+  return (function toDateString(date) {
+      var
+        code, code_i, result = "",
+        formatters = Dave_js.DateFormat.prototype;
+      
+      //convert the raw date to a Date object
+      date = new getDateObject(date);
+
+      //cycle through the format array
+      for(code_i = 0; code_i < format.length; code_i++){
+        code = format[code_i];
+        result =
+          result +
+          (typeof formatters[code] == 'function' ?
+          formatters[code](date) : code);
       }
-    }
-  };
+
+      return result;
+  });
 };
-Dave_js.DateFormatter.prototype.days = [
+
+Dave_js.DateFormat.prototype.days = [
   'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 ];
-Dave_js.DateFormatter.prototype.months = [
+Dave_js.DateFormat.prototype.months = [
   'January', 'February', 'March',     'April',   'May',      'June',
   'July',    'August',   'September', 'October', 'November', 'December'
 ];
-Dave_js.DateFormatter.prototype.yy = function yy(date){
+Dave_js.DateFormat.prototype.yy = function yy(date){
   return date.getUTCFullYear() % 2000;
 };
-Dave_js.DateFormatter.prototype.yyyy = function yyyy(date){
+Dave_js.DateFormat.prototype.yyyy = function yyyy(date){
   return date.getUTCFullYear();
 };
-Dave_js.DateFormatter.prototype.mm = function mm(date){
+Dave_js.DateFormat.prototype.mm = function mm(date){
   return date.getUTCMonth() + 1;
 };
-Dave_js.DateFormatter.prototype.MM = function MM(date){
+Dave_js.DateFormat.prototype.MM = function MM(date){
   return this.months[date.getUTCMonth()];
 };
-Dave_js.DateFormatter.prototype.dd = function dd(date){
+Dave_js.DateFormat.prototype.dd = function dd(date){
   return date.getUTCDate();
 };
-Dave_js.DateFormatter.prototype.doy = function doy(date){
+Dave_js.DateFormat.prototype.doy = function doy(date){
   return date.getUTCDate();
 };
-Dave_js.DateFormatter.prototype.dow = function dow(date){
+Dave_js.DateFormat.prototype.dow = function dow(date){
   return date.date.getUTCDay() + 1;
 };
-Dave_js.DateFormatter.prototype.DOW = function DOW(date){
+Dave_js.DateFormat.prototype.DOW = function DOW(date){
   return this.days[date.getUTCDay()];
 };
-Dave_js.DateFormatter.prototype.DD = function DD(date){
+Dave_js.DateFormat.prototype.DD = function DD(date){
   return this.days[date.getUTCDay()];
 };
-Dave_js.DateFormatter.prototype.hr = function hr(date){
+Dave_js.DateFormat.prototype.hr = function hr(date){
   var hours = date.getUTCHours();
   return  hours - (hours > 12 ? 12 : 0);
 };
-Dave_js.DateFormatter.prototype.ampm = function ampm(date){
+Dave_js.DateFormat.prototype.ampm = function ampm(date){
   var hours = date.getUTCHours();
   return  hours > 12 ? 'am' : 'pm';
 };
-Dave_js.DateFormatter.prototype.AMPM = function AMPM(date){
+Dave_js.DateFormat.prototype.AMPM = function AMPM(date){
   var hours = date.getUTCHours();
   return  hours > 12 ? 'AM' : 'PM';
 };
-Dave_js.DateFormatter.prototype.HR = function HR(date){
+Dave_js.DateFormat.prototype.HR = function HR(date){
   return date.getUTCHours();
 };
-Dave_js.DateFormatter.prototype.m = function m(date){
+Dave_js.DateFormat.prototype.m = function m(date){
   return date.getUTCMinutes();
 };
-Dave_js.DateFormatter.prototype.s = function s(date){
+Dave_js.DateFormat.prototype.s = function s(date){
   return date.getUTCSeconds();
 };
-Dave_js.DateFormatter.prototype.ms = function ms(date){
+Dave_js.DateFormat.prototype.ms = function ms(date){
   return date.getUTCMilliseconds();
 };
