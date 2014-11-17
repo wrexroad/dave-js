201c439dc20467c2c4a7c3e500a0f1de5f98298c
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2014-11-17 11:08:42 -0800

    removed autoloading stylesheet

--- dave.js
+++ dave.js
@@ -1,11 +1,1 @@
 var Dave_js = {};
-
-(function () {
-  //autoload the style sheet.
-  var bodyEl = document.getElementsByTagName("body")[0];
-  var style = document.createElement("link");
-  style.rel = "stylesheet";
-  style.type = "text/css";
-  style.href = "../css/dave-js.css";
-  bodyEl.appendChild(style);
-})();
